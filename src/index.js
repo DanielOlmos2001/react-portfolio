6d186fc056ceea7b4a75cb17ae470a9aca4ccc0d
LO504811 <siul_2010@live.com>
2025-05-21 12:07:19 -0600

    Cambio de router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // 👈 HashRouter
 import App from "./App";
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter basename="/react-portfolio">
+  <HashRouter>
     <App />
   </HashRouter>
 );
